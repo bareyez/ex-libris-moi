cbddcfcba5b6c9d162fc109433b37d74f063a785
Bryan Reyes <breyes2022@fau.edu>
2024-11-27 21:12:15 -0500

    Initial Commit

---
+++ ExLibrisMoi/ExLibrisMoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExLibrisMoiApp.swift
+//  ExLibrisMoi
+//
+//  Created by Bryan Reyes on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ExLibrisMoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
